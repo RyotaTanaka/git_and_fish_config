24ef5af94aa0a1630bd73d26aeb743d1e05385de
tanaka-careritz <tanaka.careritz@gmail.com>
2019-04-05 11:50:56 +0900

    create git config(alias) and fish config

---
+++ config.fish
@@ -0,0 +1,14 @@
+
+balias bi 'brew install'
+balias bu 'brew update'
+balias bl 'brew list'
+balias bs 'brew search'
+balias g 'git'
+balias h 'history | peco'
+
+balias vb 'vi ~/.bashrc'
+balias vgc 'vi ~/.gitconfig'
+balias vfc 'vi ~/.config/fish/config.fish'
+balias rf 'source ~/.config/fish/config.fish'
+
+balias t 'touch'
